5eb3fe8902f31e3ddf673a0f07338e9ade62aeb5
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-02-28 23:44:32 +0530

    Variables Done

---
+++ Variables/Index.java
@@ -0,0 +1,9 @@
+public class Index {
+    public static void main(String[] args) {
+        // int age = 21;
+        final int age = 21;
+        String name = "Aryan";
+
+        System.out.println("My age is " + age + " & My Name is " + name);
+    }
+}
