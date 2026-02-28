19b0825d6d7a4a1607524c8a11283267687acd85
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-02-28 23:25:10 +0530

    Datatype Done

--- DataTypes/Index.java
+++ DataTypes/Index.java
@@ -1,13 +1,19 @@
 public class Index {
     public static void main(String[] args) {
         int n = 100;
+        double x = n;
         float balance = 2456.8f;
         boolean Pan_Card = true;
         String str = "Aryan";
+        double d = 789.25;
+        int num = (int) d;
 
         System.out.println(n);
         System.out.println(balance);
         System.out.println(Pan_Card);
         System.out.println(str);
+        System.out.println(x);
+        System.out.println(d);
+        System.out.println(num);
     }
 }
