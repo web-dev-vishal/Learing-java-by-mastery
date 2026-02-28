a6841d570981a42e0be3014086fa9ef6ea7072fa
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-02-28 23:19:12 +0530

    Datatype Done

---
+++ DataTypes/Index.java
@@ -0,0 +1,13 @@
+public class Index {
+    public static void main(String[] args) {
+        int n = 100;
+        float balance = 2456.8f;
+        boolean Pan_Card = true;
+        String str = "Aryan";
+
+        System.out.println(n);
+        System.out.println(balance);
+        System.out.println(Pan_Card);
+        System.out.println(str);
+    }
+}
